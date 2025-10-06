d02e06fa3e519fc74aaff58163513d971157b8e5
Lewis Evans <lewisevansuk@gmail.com>
2025-10-06 21:50:35 +0100

    configure for deno deploy

--- serve.ts
+++ serve.ts
@@ -1,9 +1,14 @@
 import Server from "lume/core/server.ts";
+import notFound from "lume/middlewares/not_found.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
+
+server.use(notFound({
+  page404: "/404.html",
+}));
 
 server.start();
 
